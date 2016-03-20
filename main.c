142752c1ab1508c8571a8a6fcd656c3b2d8360a7
Alexandr <kutashov.alexandr@yandex.ru>
2016-03-20 23:32:14 +0300

    Add cycle and & capability

--- main.c
+++ main.c
@@ -1,44 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include "main.h"
 
-int main() 
+int main(void)
 {
-    char command[256] = "\0";
-    
-    printf("maiterm > ");
-    scanf("%s", command);
-    
-    printf("exit %d\n", exec_command(command));
-    _exit(0);
+	char command[256] = "\0";
+
+	while (1) {
+		strcpy(command, "\0");
+		printf("maiterm > ");
+		fgets(command, sizeof(command), stdin);
+		remove_substring(command, "\n");
+
+		if (strcmp(command, "exit") == 0)
+			break;
+
+		printf("exit %d\n", exec_command(command));
+	}
+
+	_exit(0);
 }
 
-pid_t exec_command(char * command)
+pid_t exec_command(char *command)
 {
-    printf("command: %s\n", command);
-    
-    pid_t pID = fork();
-    if (pID == 0)
-    {
-      printf("Child Process\n");
-      int res = execvp(command, (char *[]){ command, NULL });
-      if (res == -1)
-      {
-        printf("command not found\n");
-        _exit(1);
-      }
-    }
-    else if (pID < 0)
-    {
-        printf("Failed to fork\n");
-        _exit(1);
-    }
-    else
-    {
-      printf("Parent Process\n");
-    }
+	int waitforpid = strstr(command, " &") == NULL;
 
-    return pID;
+	if (!waitforpid)
+		remove_substring(command, " &");
+	printf("command: %s\n", command);
+
+	pid_t pID = fork();
+	int status = 0;
+
+	if (pID == 0) {
+		int res = execvp(command, (char *[]){ command, NULL });
+
+		if (res == -1)
+			printf("command not found\n");
+
+	} else if (pID < 0) {
+		printf("Failed to fork\n");
+		_exit(1);
+	} else {
+		if (waitforpid)
+			waitpid(pID, &status, 0);
+	}
+
+	return pID;
+}
+
+void remove_substring(char *s, const char *toremove)
+{
+	s = strstr(s, toremove);
+	memmove(s, s+strlen(toremove), 1+strlen(s+strlen(toremove)));
 }
--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 
-pid_t exec_command(char * command);
+pid_t exec_command(char *command);
+void remove_substring(char *s, const char *toremove);
 
 #endif
