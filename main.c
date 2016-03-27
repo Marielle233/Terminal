6302187f4dac95aab5508e36444c96c504c0086a
Alexandr <kutashov.alexandr@yandex.ru>
2016-03-27 19:19:20 +0300

    Add ctrl-c handling and passing args to functions

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "main.h"
+
+pid_t pID;
 
 int main(void)
 {
@@ -16,6 +19,9 @@ int main(void)
 		fgets(command, sizeof(command), stdin);
 		remove_substring(command, "\n");
 
+		if (strcmp(command, "") == 0)
+			continue;
+
 		if (strcmp(command, "exit") == 0)
 			break;
 
@@ -25,19 +31,32 @@ int main(void)
 	_exit(0);
 }
 
+void signal_handler(int sig)
+{
+
+	if (pID != 0)
+		printf("Parent\n");
+	else
+		printf("Child\n");
+}
+
 pid_t exec_command(char *command)
 {
 	int waitforpid = strstr(command, " &") == NULL;
 
 	if (!waitforpid)
 		remove_substring(command, " &");
-	printf("command: %s\n", command);
 
-	pid_t pID = fork();
+	pID = fork();
 	int status = 0;
 
 	if (pID == 0) {
-		int res = execvp(command, (char *[]){ command, NULL });
+		if (waitforpid)
+			signal(SIGINT, signal_handler);
+		else
+			signal(SIGINT, SIG_IGN);
+
+		int res = execvp(command, split_command(command));
 
 		if (res == -1)
 			printf("command not found\n");
@@ -46,11 +65,42 @@ pid_t exec_command(char *command)
 		printf("Failed to fork\n");
 		_exit(1);
 	} else {
+		signal(SIGINT, signal_handler);
 		if (waitforpid)
 			waitpid(pID, &status, 0);
 	}
 
 	return pID;
+}
+
+char **split_command(char *command)
+{
+
+	char **res = NULL;
+	char *p = strtok(command, " ");
+	int n_spaces = 0, i;
+
+	while (p) {
+		res = realloc(res, sizeof(char *) * ++n_spaces);
+
+		if (res == NULL) {
+			free(res);
+			_exit(-1);
+		}
+
+		res[n_spaces-1] = p;
+
+		p = strtok(NULL, " ");
+	}
+
+	res = realloc(res, sizeof(char *) * (n_spaces+1));
+	if (res == NULL) {
+		free(res);
+		_exit(-1);
+	}
+	res[n_spaces] = NULL;
+
+	return res;
 }
 
 void remove_substring(char *s, const char *toremove)
--- main.h
+++ main.h
@@ -3,7 +3,9 @@
 
 #include <sys/types.h>
 
+char **split_command(char *command);
 pid_t exec_command(char *command);
 void remove_substring(char *s, const char *toremove);
+void signal_handler(int);
 
 #endif
